72f80fc47ddf6acf66cff6544decd6722c616130
Shikhar <shikhar@joshtechnologygroup.com>
2016-03-09 00:46:07 +0530

    Too Much to write in one commit message

--- webapp/static/js/app/directives/user-list-directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('userApp').directive('userListDirective', ['PARTIALS', function (PARTIALS) {
-    return{
-        restrict: 'E',
-        templateUrl: PARTIALS.LIST
-    }
-}]);
-
